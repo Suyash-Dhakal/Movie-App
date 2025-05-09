6868df6e4613a9e887ad4e6c56d8b99c265f269a
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-05-09 23:47:53 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import SayHello from './SayHello'
+
+function App() {
+
+
+  return (
+    <>
+    <SayHello name='Suyash'/>
+    <SayHello name='Ram' />
+    </>
+  )
+}
+
+
+
+export default App
---
+++ src/SayHello.jsx
@@ -0,0 +1,4 @@
+function SayHello({name}){
+    return(<h1>Hello, {name}!</h1>);
+}
+export default SayHello
